f8dc82247ab4443a0b4f770ccdc7ceabb3c2fe8b
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-01-26 14:54:01 -0300

    feat: lógica de renderização de botão na tela inicial, implementação de useState, useRef

--- src/components/initial.js
+++ src/components/initial.js
@@ -1,8 +1,35 @@
-const Initial = () =>{
+import { useRef, useState } from 'react';
 
-    return(
+
+const Initial = () => {
+    const textInput = useRef();
+    const [showNext, setShowNext] = useState(false);
+
+    const handleChange = () => {
+        console.log(textInput.current.value.lenght)
+        if(textInput.current.value.length >= 5) setShowNext(true)
+        else setShowNext((false));
+    }
+
+    return (
         <div>
-            Initial
+            <h1>Ask a question</h1>
+            <input
+                ref={textInput}
+                onChange={handleChange}
+                name="question"
+                type="text"
+                className="form-control"
+            />
+
+            {showNext &&
+                <button
+                    className="btn"
+                >
+                    Next
+                </button>
+            }
+
         </div>
     );
 
